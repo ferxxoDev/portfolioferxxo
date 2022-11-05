0d19e13683f7262fad1e8ddc110c1c940a5f632b
ferxxoDev <fernando25mendoza95@gmail.com>
2022-11-04 23:51:25 -0400

    last ahora si commit

---
+++ envia.php
@@ -0,0 +1,30 @@
+﻿<?php
+$remitente = $_POST['email'];
+$destinatario = 'fernando25mendoza95@gmail.com'; // en esta línea va el mail del destinatario.
+$asunto = 'Form Portfolio'; // acá se puede modificar el asunto del mail
+if (!$_POST){
+?>
+
+<?php
+}else{
+	 
+    $cuerpo = "Nombre: " . $_POST["name"] . "\r\n"; 
+    // $cuerpo .= "Teléfono: " . $_POST["telefono"] . "\r\n";
+    $cuerpo .= "Correo: " . $_POST["email"] . "\r\n";
+    $cuerpo .= "Mensaje: " . $_POST["message"] . "\r\n";
+    
+	//las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo. 
+	// Si se agrega un campo al formulario, hay que agregarlo acá.
+
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST['nombre']." ".$_POST['apellido']."\" <".$remitente.">\n";
+
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include 'index.html'; //se debe crear un html que confirma el envío
+}
+?>
